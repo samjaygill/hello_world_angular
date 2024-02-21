cc9c30de74b18d862176ec1399c1170221c668ca
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-02-21 10:01:43 +0000

    week 2 complete

--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -1,34 +1,49 @@
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { AlbumsServiceService } from '../albums-service.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-albums',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, FormsModule, CommonModule],
   template: `
-  <h2>{{title}}</h2>
-  <ul>
-  <li *ngFor="let album of albums">
-  {{album}}
-  </li>
-  </ul>
-  <button (click)="onSave($event)">Save</button>
+    <h2>{{ title }}</h2>
+    {{ album.title | uppercase }}<br />
+    {{ album.artist | lowercase }}<br />
+    {{ album.rating | number : '1.2-2' }}<br />
+    {{ album.price | currency : 'GBP' }}<br />
+    {{ album.releaseDate | date : 'shortDate' }}
   `,
-  styleUrl: './albums.component.css'
+  styleUrl: './albums.component.css',
 })
 export class AlbumsComponent {
-  title = "List of Albums";
+  title = 'List of Albums';
   albums: any;
   isActive = true;
+  artist = 'Beyonce';
+  album = {
+    title: 'Take Care',
+    artist: 'Drake',
+    rating: 90.99,
+    price: 10.656,
+    releaseDate: new Date('2015, 6, 15'),
+  };
 
-  constructor(service: AlbumsServiceService){
+  constructor(service: AlbumsServiceService) {
     this.albums = service.getAlbums();
   }
 
-  onSave($event: any){
-    console.log("Button was clicked", $event);
-    
+  onSave($event: any) {
+    $event.stopPropagation();
+    console.log('Button was clicked', $event);
   }
 
+  onDivClicked() {
+    console.log('div clicked');
+  }
+
+  onKeyUp() {
+    console.log(this.artist);
+  }
 }
