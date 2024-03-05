52d0e67dabdb89f76c5777c8d87ba8a59cc80f6b
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-03-05 10:06:34 +0000

    input and output variables

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-album',
   standalone: true,
   imports: [],
   templateUrl: './album.component.html',
-  styleUrl: './album.component.css'
+  styleUrl: './album.component.css',
 })
 export class AlbumComponent {
+  @Input() details: any;
+  @Output() ownedEvent = new EventEmitter();
 
+  ownedClicked() {
+    this.ownedEvent.emit(this.details.id)
+  }
 }
--- src/app/albums/albums.component.ts
+++ src/app/albums/albums.component.ts
@@ -1,49 +1,79 @@
-import { CommonModule, NgFor } from '@angular/common';
+// import { CommonModule, NgFor } from '@angular/common';
+// import { Component } from '@angular/core';
+// import { AlbumsServiceService } from '../albums-service.service';
+// import { FormsModule } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-albums',
+//   standalone: true,
+//   imports: [NgFor, FormsModule, CommonModule],
+//   template: `
+//     <h2>{{ title }}</h2>
+//     {{ album.title | uppercase }}<br />
+//     {{ album.artist | lowercase }}<br />
+//     {{ album.rating | number : '1.2-2' }}<br />
+//     {{ album.price | currency : 'GBP' }}<br />
+//     {{ album.releaseDate | date : 'shortDate' }}
+//   `,
+//   styleUrl: './albums.component.css',
+// })
+// export class AlbumsComponent {
+//   title = 'List of Albums';
+//   albums: any;
+//   isActive = true;
+//   artist = 'Beyonce';
+//   album = {
+//     title: 'Take Care',
+//     artist: 'Drake',
+//     rating: 90.99,
+//     price: 10.656,
+//     releaseDate: new Date('2015, 6, 15'),
+//   };
+
+//   constructor(service: AlbumsServiceService) {
+//     this.albums = service.getAlbums();
+//   }
+
+//   onSave($event: any) {
+//     $event.stopPropagation();
+//     console.log('Button was clicked', $event);
+//   }
+
+//   onDivClicked() {
+//     console.log('div clicked');
+//   }
+
+//   onKeyUp() {
+//     console.log(this.artist);
+//   }
+// }
+
+import { CommonModule} from '@angular/common';
 import { Component } from '@angular/core';
-import { AlbumsServiceService } from '../albums-service.service';
-import { FormsModule } from '@angular/forms';
+import { AlbumComponent } from '../album/album.component';
 
 @Component({
   selector: 'app-albums',
   standalone: true,
-  imports: [NgFor, FormsModule, CommonModule],
+  imports: [AlbumComponent, CommonModule],
   template: `
     <h2>{{ title }}</h2>
-    {{ album.title | uppercase }}<br />
-    {{ album.artist | lowercase }}<br />
-    {{ album.rating | number : '1.2-2' }}<br />
-    {{ album.price | currency : 'GBP' }}<br />
-    {{ album.releaseDate | date : 'shortDate' }}
+    <div *ngFor="let album of albums">
+      <app-album [details]="album" (ownedEvent)="updateOwned($event)" />
+    </div>
   `,
-  styleUrl: './albums.component.css',
 })
 export class AlbumsComponent {
-  title = 'List of Albums';
-  albums: any;
-  isActive = true;
-  artist = 'Beyonce';
-  album = {
-    title: 'Take Care',
-    artist: 'Drake',
-    rating: 90.99,
-    price: 10.656,
-    releaseDate: new Date('2015, 6, 15'),
-  };
+  title = "List Of Albums"
+  albums = [
+      {id: 1, title: "Take Care", artist: "Drake", owned:true},
+      {id: 2, title: "11:11", artist: "Chris Brown", owned:true},
+      {id: 3,title: "Good Girl Gone Bad", artist: "Rihanna", owned:true},
+      {id: 4, title: "Lemonade", artist: "Beyonce", owned:false},
 
-  constructor(service: AlbumsServiceService) {
-    this.albums = service.getAlbums();
-  }
+  ]
 
-  onSave($event: any) {
-    $event.stopPropagation();
-    console.log('Button was clicked', $event);
-  }
-
-  onDivClicked() {
-    console.log('div clicked');
-  }
-
-  onKeyUp() {
-    console.log(this.artist);
+  updateOwned(id:number){
+    this.albums[id-1].owned = !this.albums[id-1].owned
   }
 }
