174fe25d8d1f0fb287e3539d83f5aef1ec61475c
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-03-20 13:15:12 +0000

    Dropdowns

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,22 +1,25 @@
-import { JsonPipe, NgIf } from '@angular/common';
+import { JsonPipe, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [FormsModule, NgIf, JsonPipe],
+  imports: [FormsModule, NgIf, NgFor, JsonPipe],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.css'
+  styleUrl: './contact-form.component.css',
 })
 export class ContactFormComponent {
+  contactMethods = [
+    { id: 1, name: 'Email' },
+    { id: 2, name: 'Phone' },
+  ];
 
-  log(x:any){
+  log(x: any) {
     console.log(x);
   }
 
-  submit(f: any){
+  submit(f: any) {
     console.log(f);
-    }
-
+  }
 }
