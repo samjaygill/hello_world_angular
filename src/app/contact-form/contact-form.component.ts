b89303ae3363f6e0b2e83650c79a7913b4a12c84
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-03-20 12:21:26 +0000

    ngModel

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
 
+  log(x:any){
+    console.log(x);
+  }
+  
 }
