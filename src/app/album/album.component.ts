7bf80671cd071dfc46bfa205bc10ef5dca829568
samjaygill <138401809+samjaygill@users.noreply.github.com>
2024-03-20 11:15:36 +0000

    ngif

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,9 +1,10 @@
+import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-album',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './album.component.html',
   styleUrl: './album.component.css',
 })
